2c9a4478a4c6ba0e685eaa60a7d1182f646ee19c
jvmoliveira <jvmo@ic.ufal.br>
2021-01-21 11:47:43 -0300

    Fix Printboard and printpiece in UI

--- src/application/UI.java
+++ src/application/UI.java
@@ -52,17 +52,6 @@ public class UI {
         }
     }
 
-    public static void printBoard(ChessPiece[][] pieces) {
-        for (int i=0; i<pieces.length; i++) {
-            System.out.print((8 - i) + " ");
-            for (int j =0; j< pieces.length; j++) {
-                printPiece(pieces[i][j], false);
-            }
-            System.out.println();
-        }
-        System.out.println("  a b c d e f g h");
-    }
-
     public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
         printBoard(chessMatch.getPieces());
         System.out.println();
@@ -81,6 +70,16 @@ public class UI {
             }
     }
 
+    public static void printBoard(ChessPiece[][] pieces) {
+        for (int i=0; i<pieces.length; i++) {
+            System.out.print((8 - i) + " ");
+            for (int j =0; j< pieces.length; j++) {
+                printPiece(pieces[i][j], false);
+            }
+            System.out.println();
+        }
+        System.out.println("  a b c d e f g h");
+    }
 
     public static void printBoard(ChessPiece[][] pieces, boolean [][] possibleMoves) {
         for (int i=0; i<pieces.length; i++) {
@@ -99,7 +98,7 @@ public class UI {
             System.out.print(ANSI_BLUE_BACKGROUND);
         }
         if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         }
         else {
             if (piece.getColor() == Color.WHITE) {
